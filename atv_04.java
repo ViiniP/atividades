7acb18ae39af543a0703ea7ed7ec5a4b0159bad1
ViiniP <107128687+ViiniP@users.noreply.github.com>
2022-07-11 08:31:36 -0300

    Add files via upload

---
+++ atv_04.java
@@ -0,0 +1,12 @@
+public class atv_04 {
+    public static void main(String[] args) {
+        int i = 0;
+
+        System.out.println(1 > 2);
+        System.out.println(8 == 8);
+        System.out.println((12 - 5) > 6);
+        System.out.println((0 < 3) && (8 < 9));
+        System.out.println((i++) > i);
+        System.out.println(((10 * 90) / (50 - 2)) == 16);
+    }
+}
